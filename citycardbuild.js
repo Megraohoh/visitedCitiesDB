7ec90c7833384e5f725af9ba6cbe94dc9325393e
Meghan Debity <mdebity@gmail.com>
2018-04-16 13:07:32 -0500

    adding notes to new js file

---
+++ citycardbuild.js
@@ -0,0 +1,4 @@
+//ON THIS FILE IS CARD CREATION
+//Build city cards in this format: city name, image, year visited, and attractions last
+//Be able to filter by property
+//Include style by property
