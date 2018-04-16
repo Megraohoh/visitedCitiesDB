5d7fbe6b01c030f3e4e78b8d1cb74704c99730a8
Meghan Debity <mdebity@gmail.com>
2018-04-16 13:00:00 -0500

    started listing city details

--- database.js
+++ database.js
@@ -1,5 +1,76 @@
-//IN THIS FILE
+//IN THIS FILE IS DATABASE CREATION
+
 //City database creation
+const VisitedCitiesDatabase = {}
+
 //City table in database
+VisitedCitiesDatabase.cities = []
+
 //Add cities to the city table in the database
+VisitedCitiesDatabase.cities.push(
+    {
+        city: "Berlin",
+        year_visited: "2014, 2016",
+        img: "",
+        five_attractions: "",
+    },
+    {
+        city: "Paris",
+        year_visited: "2016",
+        img: "",
+        five_attractions: "",
+    },
+    {
+        city: "Dublin",
+        year_visited: "2016",
+        img: "",
+        five_attractions: "",
+    },
+    {
+        city: "Tonder",
+        year_visited: "2016",
+        img: "",
+        five_attractions: "",
+    },
+    {
+        city: "Liverpool",
+        year_visited: "2016",
+        img: "",
+        five_attractions: "",
+    },
+    {
+        city: "Rome",
+        year_visited: "2014",
+        img: "",
+        five_attractions: "",
+    },
+    {
+        city: "Barcelona",
+        year_visited: "2014",
+        img: "",
+        five_attractions: "",
+    },
+    {
+        city: "Milan",
+        year_visited: "2014",
+        img: "",
+        five_attractions: "",
+    },
+    {
+        city: "Savannah",
+        year_visited: "2014, 2016",
+        img: "",
+        five_attractions: "",
+    },
+    {
+        city: "New Orleans",
+        year_visited: "2014, 2016",
+        img: "",
+        five_attractions: "",
+    }
+)
 //Save database to local storage
+localStorage.setItem(
+    "VisitedCitiesDatabase",
+    JSON.stringify(VisitedCitiesDatabase)
+)
--- main.js
+++
@@ -1,5 +0,0 @@
-//IN THIS FILE
-//City database creation
-//City table in database
-//Add cities to the city table in the database
-//Save database to local storage
